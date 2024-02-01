ed971c71811f336df2055e97b2162df0c24a76ba
ectionboy <125730248+ectionboy@users.noreply.github.com>
2024-02-01 23:38:45 +0200

    add Routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./Home/Home";
+import Catalog from "./Catalog/Catalog";
+import Layout from "./Layout/Layout";
+import Favorites from "./Favorites/Favorites";
+import NotFound from "./NotFound/NotFound";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route index element={<Home />} />
+          <Route path="/catalog" element={<Catalog />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />      
+      </Routes>
   );
 };
