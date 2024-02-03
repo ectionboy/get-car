7c5307fb2703275da18536ad1f5b31efa0c79d70
ectionboy <125730248+ectionboy@users.noreply.github.com>
2024-02-03 22:22:39 +0000

    Deploying to gh-pages from @ ectionboy/get-car@2c4397b7e36306fb694c4cdc3dfd2116648544ec 🚀

---
+++ static/js/296.9653db25.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkget_car=self.webpackChunkget_car||[]).push([[296],{409:function(n,t,r){r.r(t);r(791);var e=r(420),u=r(351),c=r(184);t.default=function(){var n=(0,e.v9)(u.Rn);return console.log(n),(0,c.jsx)("div",{children:"Catalog"})}},351:function(n,t,r){r.d(t,{Rn:function(){return e},_l:function(){return u}});var e=function(n){return n.cars.items},u=function(n){return n.favorites.items}}}]);
+//# sourceMappingURL=296.9653db25.chunk.js.map
