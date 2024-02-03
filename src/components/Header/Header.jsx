c9d03953613afa6532f1208ca201c16543848413
ectionboy <125730248+ectionboy@users.noreply.github.com>
2024-02-04 00:45:35 +0200

    add Header styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,63 @@
+import { Box, Button, Container, Divider } from '@mui/material';
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const navToHomePage = () => {
+    navigate('/');
+  };
+  const navToCatalogPage = () => {
+    navigate('/catalog');
+  };
+  const navToFavoritesPage = () => {
+    navigate('/favorites');
+  };
+
   return (
-    <header>
-      <nav>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/catalog">Catalog</NavLink>
-        <NavLink to="/favorites">Favorites</NavLink>
-      </nav>
-    </header>
+    <>
+      <Container
+        component="header"
+        sx={{
+          display: 'flex',
+          alignItems: 'baseline',
+          justifyContent: 'space-between',
+          padding: '8px 0',
+        }}
+      >
+        <Box component="nav">
+          <Button
+            sx={{}}
+            component="a"
+            onClick={navToHomePage}
+            type="button"
+            size="small"
+          >
+            Home
+          </Button>
+          <Button
+            sx={{}}
+            component="a"
+            onClick={navToCatalogPage}
+            type="button"
+            size="small"
+          >
+            Catalog
+          </Button>
+          <Button
+            sx={{}}
+            component="a"
+            onClick={navToFavoritesPage}
+            type="button"
+            size="small"
+          >
+            Favorites
+          </Button>
+        </Box>
+      </Container>
+      <Divider />
+    </>
   );
 };
 
