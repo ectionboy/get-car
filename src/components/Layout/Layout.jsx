c4dfee00f0cd5b67003fb8a7c3933faebb7a3638
ectionboy <ection.boy@gmail.com>
2024-02-04 22:35:23 +0200

    v1

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,4 +1,3 @@
-// import Footer from 'components/Footer/Footer';
 import Header from 'components/Header/Header';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
@@ -10,7 +9,6 @@ const Layout = () => {
       <main>
         <Outlet />
       </main>
-      {/* <Footer /> */}
     </>
   );
 };
