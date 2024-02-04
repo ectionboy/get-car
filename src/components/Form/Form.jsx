bb187b9b8eceb8e09a6d9a2b016257f7b5e03a7c
ectionboy <125730248+ectionboy@users.noreply.github.com>
2024-02-04 04:02:49 +0200

    add filter

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,146 +1,79 @@
-import { Box, Button, FormControl, InputBase, InputLabel, MenuItem, Select, TextField, styled,  } from '@mui/material'
-import React, { useState } from 'react'
-import { nanoid } from 'nanoid'
-import makes from '../../data/makes.json';
-import prices from '../../data/prices.json';
-
-
-const BootstrapInput = styled(InputBase)(({ theme }) => ({
-    'label + &': {
-      marginTop: theme.spacing(3),
-    },
-    '& .MuiInputBase-input': {
-      borderRadius: 4,
-      position: 'relative',
-      backgroundColor: theme.palette.background.paper,
-      border: '1px solid #ced4da',
-      fontSize: 16,
-      padding: '10px 26px 10px 12px',
-      transition: theme.transitions.create(['border-color', 'box-shadow']),
-      fontFamily: [
-        '-apple-system',
-        'BlinkMacSystemFont',
-        '"Segoe UI"',
-        'Roboto',
-        '"Helvetica Neue"',
-        'Arial',
-        'sans-serif',
-        '"Apple Color Emoji"',
-        '"Segoe UI Emoji"',
-        '"Segoe UI Symbol"',
-      ].join(','),
-      '&:focus': {
-        borderRadius: 4,
-        borderColor: '#80bdff',
-        boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
-      },
-    },
-  }));
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { getCars } from '../../redux/selectors';
+import { useDispatch } from 'react-redux';
+import { setFilter } from '../../redux/filter/slice';
+import makes from '../../data/makes';
+import prices from '../../data/prices';
+import { FormEl, Input, Label, Select, SelectPrice } from './Form.styled';
+import { nanoid } from 'nanoid';
 
 const Form = () => {
-    const [selectedMake, setSelectedMake] = useState('Enter the text');
-    const [selectedPrice, setSelectedPrice] = useState('To $');
+  const dispatch = useDispatch();
+  const adverts = useSelector(getCars);
+  const [filteredAdverts, setFilteredAdverts] = useState([]);
+  const [selectedMake, setSelectedMake] = useState('Enter the text');
+  const [selectedPrice, setSelectedPrice] = useState('To $');
 
-console.log(selectedMake)
-console.log(selectedPrice)
+  const handleMakeSelectChange = event => {
+    setSelectedMake(event.target.value);
+  };
 
+  const handlePriceSelectChange = event => {
+    setSelectedPrice(event.target.value);
+  };
 
-    const handleMakeSelectChange = event => {
-        setSelectedMake(event.target.value);
-      };
-      const handlePriceSelectChange = event => {
-        setSelectedPrice(event.target.value);
-      };
+  const handleOnSubmit = event => {
+    event.preventDefault();
+    setFilteredAdverts(adverts.filter(advert => advert.make === selectedMake));
+    dispatch(setFilter(filteredAdverts));
+  };
 
   return (
-    <>
-    <Box sx={{ minWidth: 320 }} component="form">
-    <FormControl sx={{ m: 1 }} variant="standard">
-        <InputLabel id="models-select-label">Car brand</InputLabel>
+    <FormEl onClick={handleOnSubmit}>
+      <Label>
+        Car brand
         <Select
-        sx={{ minWidth: 320 }}
-          labelId="models-select-label"
-          id="models-select"
+          name="models"
           value={selectedMake}
           onChange={handleMakeSelectChange}
-          input={<BootstrapInput />}
-          renderValue={(selected) => {
-            if (selected === 'Enter the text') {
-              return <em>Enter the text</em>;
-            }
-            return selected;
-
-          }}
-        > 
-  <MenuItem disabled value="">
-            <em>Enter the text</em>
-          </MenuItem>
-        {makes.map(make => (
-            <MenuItem key={nanoid()} value={make}>
+        >
+          <option value="Enter the text">Enter the text</option>
+          {makes.map(make => (
+            <option key={nanoid()} value={make}>
               {make}
-            </MenuItem>
+            </option>
           ))}
         </Select>
-        </FormControl>
-
-
-
-
-
-        <FormControl sx={{ m: 1 }} variant="standard">
-
-
-        <InputLabel id="prices-select-label">Price/ 1 hour</InputLabel>
-        <Select
-        sx={{ minWidth: 320 }}
-          labelId="prices-select-label"
-          id="prices-select"
+      </Label>
+      <Label>
+        Price/ 1 hour
+        <SelectPrice
+          name="prices"
           value={selectedPrice}
           onChange={handlePriceSelectChange}
-          input={<BootstrapInput />}
-          renderValue={(selected) => {
-            if (selected === 'Enter the text') {
-              return <em>To $</em>;
-            }
-            return selected;
-
-          }}
-        > 
-  <MenuItem disabled value="">
-            <em>To $</em>
-          </MenuItem>
-        {prices.map(make => (
-            <MenuItem key={nanoid()} value={make}>
-              {make}
-            </MenuItem>
+        >
+          <option value="To $" disabled>
+            To $
+          </option>
+          {prices.map(price => (
+            <option key={nanoid()} value={price}>
+              {price}
+            </option>
           ))}
-        </Select>
-          
-      </FormControl>
-      <FormControl sx={{ m: 1 }} variant="standard">
+        </SelectPrice>
+      </Label>
+      <Label>
+        Car mileage / km
+        <Input placeholder="From" />
+      </Label>
+      <Label>
+        <Input placeholder="To" />
+      </Label>
+      <Label></Label>
+      <button type="submit">Search</button>
+    </FormEl>
+  );
+};
 
-
-      <TextField
-      
-          id="mileage-input-from"
-          label="Сar mileage / km"
-          type="number"
-        />
-        </FormControl>
-        <FormControl sx={{ m: 1 }} variant="standard">
-
-
-<TextField
-
-    id="mileage-input-to"
-    label="Сar mileage / km"
-    type="number"
-  />
-  </FormControl>
-        <Button variant="contained" type="submit">Search</Button>
-
-  </Box>
-  </>)
-}
-
-export default Form
+export default Form;
---
+++ src/components/Form/Form.styled.js
@@ -0,0 +1,59 @@
+import styled from 'styled-components';
+
+export const FormEl = styled.form`
+  display: flex;
+  justify-content: center;
+  align-items: end;
+  gap: 18px;
+`;
+export const Label = styled.label`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  color: #8a8a89;
+  font-weight: 500;
+  line-height: 18px;
+`;
+
+export const Select = styled.select`
+  color: var(--color-text-main);
+  font-weight: 500;
+  line-height: 20px;
+  font-family: Manrope;
+  padding-top: 14px;
+  padding-bottom: 14px;
+  padding-left: 18px;
+  padding-right: 89px;
+  background-color: var(--color-back-second);
+  border: none;
+  outline: none;
+  border-radius: 14px;
+
+  &:focus {
+    outline: 1px solid var(--color-accent);
+  }
+`;
+
+export const Input = styled.input`
+  color: var(--color-text-main);
+  font-weight: 500;
+  line-height: 20px;
+  width: 160px;
+  font-family: Manrope;
+  padding-top: 14px;
+  padding-bottom: 14px;
+  padding-left: 18px;
+  padding-right: 89px;
+  background-color: var(--color-back-second);
+  border: none;
+  outline: none;
+  border-radius: 14px;
+
+  &:focus {
+    outline: 1px solid var(--color-accent);
+  }
+`;
+
+export const SelectPrice = styled(Select)`
+  padding-right: 30px;
+`;
